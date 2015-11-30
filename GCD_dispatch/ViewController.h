2ce0eda44eb09b7fd4406907f9e161595ed8a67d
Administrator <Administrator@administratordemacbook-air.local>
2015-11-30 14:01:10 +0800

    Initial Commit

---
+++ GCD_dispatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD_dispatch
+//
+//  Created by Administrator on 15/11/30.
+//  Copyright © 2015年 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCD_dispatch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD_dispatch
+//
+//  Created by Administrator on 15/11/30.
+//  Copyright © 2015年 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
